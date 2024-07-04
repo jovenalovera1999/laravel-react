b80438e57d343654468bb0d89646a30d0101871c
unknown <DevHelloThere>
2024-07-05 00:27:50 +0800

    Update not working

--- backend/app/Http/Controllers/Api/StudentController.php
+++ backend/app/Http/Controllers/Api/StudentController.php
@@ -8,6 +8,17 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
+    public function index()
+    {
+        $students = Student::orderBy('first_name', 'asc')
+            ->get();
+
+        return response()->json([
+            'status' => 200,
+            'students' => $students
+        ]);
+    }
+
     public function store(Request $request)
     {
         Student::create([
@@ -19,6 +30,28 @@ class StudentController extends Controller
         return response()->json([
             'status' => 200,
             'message' => 'Student successfully created.'
+        ]);
+    }
+
+    public function edit($student_id)
+    {
+        $student = Student::find($student_id);
+        return response()->json([
+            'status' => 200,
+            'student' => $student
+        ]);
+    }
+
+    public function update(Request $request, Student $student)
+    {
+        $student->update([
+            'first_name' => $request->first_name,
+            'middle_name' => $request->middle_name,
+            'last_name' => $request->last_name
+        ]);
+
+        return response()->json([
+            'status' => 200
         ]);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,7 +16,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::controller(StudentController::class)->group(function () {
-    Route::post('/store/student', 'store');
+    Route::get('/students', 'index');
+    Route::get('/student/edit/{student_id}', 'edit');
+
+    Route::post('/student/store', 'store');
+    Route::put('/student/update/{student_id}', 'update');
 });
 
 
